502f04da0b0bf27b2e983bc3098cb4bcab52bc41
Sean Xiao <jean.paul.1685@outlook.com>
2020-02-06 16:01:14 -0500

    Complete list detail display

---
+++ src/app/hero.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { hero } from './hero';
+import { HEROS } form './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+}
